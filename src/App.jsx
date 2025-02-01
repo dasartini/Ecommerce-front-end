25bd02512a86230467a9133e498c4040e00e1aa0
dasartini <adriansartinid@gmail.com>
2025-02-01 14:39:48 +0000

    added some of the homescreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import ProductsGrid from './components/ProductsGrid'
+import HomePage from './components/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Header/>
+    <HomePage/>
     </>
   )
 }
