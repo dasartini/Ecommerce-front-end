5139304ebe2eccc0455e4b256bbda20283143fb4
dasartini <adriansartinid@gmail.com>
2025-02-02 23:17:16 +0000

    fixed navbar issues

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -5,8 +5,8 @@ export default function AllProducts() {
 
 
   return (
-    <div>
+    <>
     <ProductsGrid/>
-    </div>
+    </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,37 @@
 import HeaderStyle from "../styles/HeaderStyle";
-import logo from "../assets/logo.png"
-import basket from "../assets/basket.svg"
+import logo from "../assets/logo.png";
+import basket from "../assets/basket.svg";
 import { Link } from "react-router";
 import { useBasketContext } from "../contexts/BasketContext";
 
 export default function Header() {
-  const {quantity} = useBasketContext()
+  const { quantity } = useBasketContext();
+
   return (
     <HeaderStyle>
-    <header>
+      <header>
         <div className="mainHeaderContainer">
-   <div className="logoContainer">
-    <img className="logo" src={logo}/>
-    <div className="buttonsContainer">
-   <Link to="/shop"> <p>Shop</p></Link>
-<p>About</p>
-<p>Contact</p>
-    </div>
+          <div className="centerSection">
+            <img className="logo" src={logo} alt="Shop Logo" />
+            <nav className="buttonsContainer">
+              <Link to="/shop">
+                <p>Shop</p>
+              </Link>
+             <Link to={"/"}> <p>About</p></Link>
+              <p>Contact</p>
+            </nav>
+          </div>
 
-   </div>
-   <Link to={"/basket"}>
-   <div className="shoppingBasket">
-<img className="basket" src={basket}/> {quantity}
-   </div>
-   </Link>
-   </div>
-    </header>
+        
+            <Link to={"/basket"}>
+              <div className="shoppingBasket">
+                <img className="basket" src={basket} alt="Basket Icon" />
+                <span className="basketQuantity">{quantity}</span>
+              </div>
+            </Link>
+          
+        </div>
+      </header>
     </HeaderStyle>
-  )
-  
+  );
 }
